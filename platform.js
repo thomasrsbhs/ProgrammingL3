bde45cb645a9ae10765cbeeee1fe47bebc4c2a1f
thomasrsbhs <51344420+thomasrsbhs@users.noreply.github.com>
2021-11-23 05:37:09 +1300

    Add files via upload
    improvements to collision, platforms spawning, player positioning and lives and score counting

--- platform.js
+++ platform.js
@@ -1,49 +1,65 @@
-class Platform {
-    constructor(x, y, w, h, c, xSpeed, ySpeed) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.c = c;
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
-        
+class Platform { //platform class vars for jumper.html x,y,w,h,c & platYspeed
+  constructor(x, y, w, h, c, ySpeed) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+    this.ySpeed = ySpeed;
+
+  }
+
+  draw() { //draws platforms calling platform class vars 
+    canvasContext.fillStyle = this.c;
+    canvasContext.fillRect(this.x, this.y, this.w, this.h);
+  }
+
+  Move() {
+    this.y += this.ySpeed; // obstacles move down
+    // this.x += this.xSpeed; // obstacles move across 
+
+    if (this.x + this.w > canvas.width) {
+      this.xSpeed *= -1;
+    }
+    if (this.x < 0) {
+      this.xSpeed *= -1;
+    }
+    if (this.y > canvas.height) { //when platform reaches bottom of screen respawns at top
+      this.w = 70; //platform width
+      this.h = 10; //platform height
+      this.x = Math.floor(Math.random() * (canvas.width - this.w)); //randomised platXpos spawn
+      // this.xSpeed = Math.floor(Math.random()*(6 - 1) + 1);
+      this.ySpeed = Math.floor(Math.random() * (3 - 1) + 2); //randomised platYspeed 
+
+      this.y = 0 - this.h;
     }
 
-    drawRect() {
-        canvasContext.fillStyle = this.c;
-        canvasContext.fillRect(this.x, this.y, this.w, this.h);
-      }
-    
-      platformMove() {
-        this.y += this.ySpeed; // obstacles move down
-        // this.x += this.xSpeed; // obstacles move across 
-    
-        if(this.x + this.w > canvas.width){
-          this.xSpeed *= -1;
-        }
-        if(this.x < 0){
-          this.xSpeed *= -1;
-        }
-        if(this.y > canvas.height){
-          this.w = 100;
-          this.h = 10;
-          this.x = Math.floor(Math.random()*(canvas.width - this.w));
-          this.xSpeed = Math.floor(Math.random()*(6 - 1) + 1);
-          this.ySpeed = Math.floor(Math.random()*(10 - 2) + 2);
-    
-          this.y = 0 - this.h;
-        }
-    
+  }
+
+  hit() { //for platform collision of player
+
+    if (this.y + this.h > player.y && this.y < player.y + player.h && this.x + this.w > player.x && this.x < player.x + player.w) {
+      // spacePressed = true;
+      player.y -= player.ySpeed;
+      score += 1;
+
+      if (player.y < 0 + player.h) { // stops player going above screen 
+        player.yVel = 0;
+        player.ySpeed = 0;
+      } else { // when not touching top screen run true 
+        player.yVel = player.thrust;
+        player.ySpeed = 5;
       }
 
-      // playerDeath(){ //player collision
-      //   return (this.x + this.w > player.x && this.x < player.x + player.w) && (this.y + this.h > player.y && this.y < player.y + player.h);
-      // }
-    
+      if (player.y > 0) {
+        player.yVel = player.thrust;
+      } else {
+        player.yVel = 0;
+      }
+    }
 
 
-
+  }
 
 
 
