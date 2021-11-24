375197730ba546efce82e6ad17708ac4710e3a69
thomasrsbhs <51344420+thomasrsbhs@users.noreply.github.com>
2021-11-25 00:28:17 +1300

    Add files via upload

--- jumper.js
+++ jumper.js
@@ -1,5 +1,5 @@
 class Player {
-  constructor(x, y, w, h, c, xSpeed, ySpeed, yVel, thrust) {
+  constructor(x, y, w, h, c, xSpeed, ySpeed) {
     this.x = x;
     this.y = y;
     this.w = w;
@@ -7,63 +7,42 @@ class Player {
     this.c = c;
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
-    this.yVel = yVel;
-    this.thrust = thrust;
 
-    // this.gravity = gravity;
-    // this.gravitySpeed = gravitySpeed;
-  } //end func
-
-  gravity() {
-    // this.gravitySpeed += this.y;
-    // this.y += this.speedY + this.gravitySpeed;
-  } //end func
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+  }
+  
+  this.newPos = function() {
+    this.gravitySpeed += this.gravity;
+    this.y += this.speedY + this.gravitySpeed;
+  }
 
   drawRect() {
     canvasContext.fillStyle = this.c;
     canvasContext.fillRect(this.x, this.y, this.w, this.h);
-  } //end func
+  }
 
-  
 
   playerMove() {
 
-    this.y += this.yVel; //gravity
-    this.thrust = -12.5 ;
-    this.yVel += 0.4;
-    // this.thrust += -5; //gravity
-    console.log(this.yVel)
-
-    if(rightPressed) {
+    if (rightPressed) {
       this.x += this.xSpeed;
-      if(this.x > canvas.width){ // once out of right screen wrap to left 
+      if (this.x > canvas.width) { // once out of right screen wrap to left 
         this.x = 0;
       }
     }
-    if(leftPressed){
+    if (leftPressed) {
       this.x -= this.xSpeed;
       if (this.x < 0) { // once out of left side of screen wrap to right
         this.x = canvas.width - this.w;
       }
     }
-    if(spacePressed && this.y > 0) { // cant go out of top of screen
-      // this.y -= this.ySpeed;
-      this.yVel = this.thrust;
+    if (spacePressed && this.y > 0) { // cant go out of top of screen
+      this.y -= this.ySpeed;
+
+      if (spacePressed = false && this.y > 0) {
+        this.y += this.ySpeed;
+      }
     }
 
-    if(this.y > canvas.height){ //player fall loop
-      this.y = 0 + this.h;
-      this.x = canvas.width / 2 - this.h;
-      // this.yVel = this.thrust;
-      // this.ySpeed = 5;
-    }
-    if(spacePressed = false && this.y > 0){
-      this.ySpeed = 0;
-      this.thrust = 0;
-      this.yVel = 0;
-      // this.y += this.ySpeed;
-      
-    }
-
-  }
 }
